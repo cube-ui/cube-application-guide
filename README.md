a761c1a9a3a857c25ee9ad9707968db211704ee3
AmyFoxFN <funan_amy@foxmail.com>
2018-03-01 23:39:17 +0800

    <add> init cube-template

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # cube-application-guide
-A guild for cube application
+
+> A guide for cube application
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+// By default we import all the components.
+// Only reserve the components on demand and remove the rest.
+// Style is always required.
+import {
+  /* eslint-disable no-unused-vars */
+  Style,
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  Loading,
+  Tip,
+  Popup,
+  Toast,
+  Picker,
+  TimePicker,
+  Dialog,
+  ActionSheet,
+  Scroll,
+  Slide,
+  IndexList
+} from 'cube-ui'
+import App from './App'
+
+Vue.use(Button)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Loading)
+Vue.use(Tip)
+Vue.use(Toast)
+Vue.use(Picker)
+Vue.use(TimePicker)
+Vue.use(Dialog)
+Vue.use(ActionSheet)
+Vue.use(Scroll)
+Vue.use(Slide)
+Vue.use(IndexList)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
