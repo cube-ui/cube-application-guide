4b6b36d78cf468b54c4d211eb8c55e68a34c24a5
AmyFoxFN <funan_amy@foxmail.com>
2018-03-02 10:08:03 +0800

    <add> main layout + slide

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="header">NBA</div>
+    <div class="navigator">
+      <ul class="tab-list">
+        <li v-for="(item, index) in tabList" :key="index"
+          @click="switchTab(index)" :class="{'active': currentPage === index}">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <cube-slide
+        :data="tabList"
+        :loop="false"
+        :autoPlay="false"
+        :threshold="0.1"
+        @change="slideChange()"
+        >
+        <cube-slide-item v-for="(item, index) in tabList" :key="index">
+          <div class="match-list-wrapper">
+            <match-list></match-list>
+          </div>
+        </cube-slide-item>
+        <div slot="dots"></div>
+      </cube-slide>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MatchList from './components/match-list'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      currentPage: 1,
+      tabList: ['已结束', '未结束', '我的关注']
+    }
+  },
   components: {
-    HelloWorld
+    MatchList
+  },
+  methods: {
+    switchTab (index) {
+      this.currentPage = index
+      console.log(index)
+    },
+    slideChange (index) {
+      console.log(index)
+      this.currentPage = index
+    }
   }
 }
 </script>
 
 <style>
+html, body, #app {
+  height: 100%
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.content {
+  height: calc(100% - 85px);
+  overflow: hidden;
+}
+.header {
+  height: 44px;
+  line-height: 44px;
+  background-color: #ea3b32;
+  color: white;
+}
+.tab-list {
+  height: 40px;
+  line-height: 44px;
+  display: flex;
+  justify-content: space-around;
+  border-bottom: solid 1px #f0f0f1;
+}
+.active {
+  color: orange;
 }
 </style>
---
+++ src/components/match-list.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="match-list">
+    <cube-scroll
+      :data="matchList">
+      <ul>
+        <li v-for="(item, index) in matchList" :key="index">
+          {{ item }}
+        </li>
+      </ul>
+    </cube-scroll>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'match-list',
+  data () {
+    return {
+      matchList: ['比赛1', '比赛2']
+    }
+  }
+}
+</script>
+
+<style>
+.match-list {
+
+}
+</style>
